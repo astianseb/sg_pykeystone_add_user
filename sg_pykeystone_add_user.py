7d6616c559160d649a67823334f07f60257c1024
astianseb <callmeseb@gmail.com>
2014-04-04 14:28:20 +0200

    Made output more verbose by leverage of keystoneclient.v2.0.shell library.

--- sg_pykeystone_add_user.py
+++ sg_pykeystone_add_user.py
@@ -1,21 +1,22 @@
 __author__ = 'sebastian'
 
 from keystoneclient.v2_0 import client
+from keystoneclient.v2_0 import shell
 import csv
 import string
 import random
 import sys
 import os
 
-USER = os.environ['OS_USERNAME']
-PASS = os.environ['OS_PASSWORD']
-TENANT_NAME = os.environ['OS_TENANT_NAME']
-KEYSTONE_URL = os.environ['OS_AUTH_URL']
+#USER = os.environ['OS_USERNAME']
+#PASS = os.environ['OS_PASSWORD']
+#TENANT_NAME = os.environ['OS_TENANT_NAME']
+#KEYSTONE_URL = os.environ['OS_AUTH_URL']
 
-#USER = "admin"
-#PASS = "0641540afdcb4e91"
-#TENANT_NAME = "admin"
-#KEYSTONE_URL = "http://172.29.68.13:35357/v2.0/"
+USER = "admin"
+PASS = "0641540afdcb4e91"
+TENANT_NAME = "admin"
+KEYSTONE_URL = "http://172.29.68.13:35357/v2.0/"
 CSVFILE = "user_names_emails.csv"
 
 BULK_TENANT = 'openstack'
@@ -68,6 +69,44 @@ class SGkeystone:
         else:
             return False
 
+#
+#
+#
+class Namespace:
+    ''' Fake class needed to create a namespace for shell library. In normal
+      keystoneclient use - number of parameters are passed in command line. Keystoneclient
+      is leveraging argparse - and argparse is using namespace as a data structure. In here
+      we're using only part of parameters. The others are:
+      self.debug = False
+      self.force_new_token = False
+      self.func = <function do_user_list at 0x2909b18>
+      self.help = False
+      self.insecure = False
+      self.os_cacert = None
+      self.os_cache = False
+      self.os_cert = ''
+      self.os_endpoint = ''
+      self.os_identity_api_version = ''
+      self.os_key = ''
+      self.os_region_name = ''
+      self.os_tenant_id = ''
+      self.os_token = ''
+      self.os_username = 'admin'
+      self.stale_duration = 30
+      self.tenant_id = None
+      self.timeout = 600
+    '''
+
+    def __init__(self, user=None, tenant=None):
+        self.tenant = tenant
+        self.os_auth_url = KEYSTONE_URL
+        self.os_password = PASS
+        self.os_tenant_name = TENANT_NAME
+        self.os_username = USER
+        self.user = user
+
+args = Namespace()
+
 try:
     with open(CSVFILE) as csvfile:
         try:
@@ -92,8 +131,10 @@ if not obj.is_tenant_configured(BULK_TENANT):
     obj.keystone.tenants.create(tenant_name=BULK_TENANT,
                         description=BULK_TENANT_DESC, enabled=True)
     print "Tenant %s created" % BULK_TENANT
+    shell.do_tenant_get(kc, Namespace(tenant=BULK_TENANT))
 else:
     print 'Tenant %s already exist!' % BULK_TENANT
+    shell.do_tenant_get(kc, Namespace(tenant=BULK_TENANT))
 
 
 with open(CSVFILE) as csvfile:
@@ -112,5 +153,10 @@ with open(CSVFILE) as csvfile:
                                       email=user_email,
                                       tenant_id=obj._get_tenant_id(BULK_TENANT))
             print "User %s created. u:%s p:%s" % (user_id, user_id, user_pass)
+            shell.do_user_get(kc,Namespace(user=user_id))
         else:
             print "User %s already exist!" % user_id
+            shell.do_user_get(kc,Namespace(user=user_id))
+
+    print "List of users available under tenant: %s" %BULK_TENANT
+    shell.do_user_list(kc,Namespace(tenant=BULK_TENANT))
